9dea5a1da3d6ffa4e854a732f001bf29f98b52af
PriscilaCodrington <priscilacodrington@gmail.com>
2023-03-31 14:24:44 +0200

    Exercise Complete: API Dog

--- main.js
+++ main.js
@@ -4,17 +4,34 @@ function getDogs() {
   // you may have to use Object.keys() method...
   // each breed should then be the title of the card and the alt of the image (which will come in the following task)
   // the code for the card has already been written for you
+    fetch('https://dog.ceo/api/breeds/list/all')
+        .then(response => response.json())
+        .then(data => {
+          
+            console.log(Object.keys(data.message));
+            const breedNames = Object.keys(data.message);
+            console.log(breedNames)
 
-  const breedNames = []
+              breedNames.forEach(breed => {
+               fetch(`https://dog.ceo/api/breed/${breed}/images/random`)
+               .then(response => response.json())
+               .then(data =>{
+                    let dogImg=data.message
+                    const container = document.querySelector('.container-grid');
 
-  const container = document.querySelector('.container-grid');
+                container.innerHTML += `
+               <div class="card-body">
+               <img class="card-img-top" src="${dogImg}" alt="${breed}" style="object-fit: cover; object-position: center;" height="300px" />
+               <h5 class="card-title">${breed}</h5>
+               </div>
+               </div>`
+               
+               })
+          })
 
-  container.innerHTML = breedNames.map(breed => `
-     <div class="card-body">
-     <img class="card-img-top" src="" alt="" style="object-fit: cover; object-position: center;" height="300px" />
-     <h5 class="card-title">Breed name</h5>
-     </div>
-     </div>`).join('');
+  })
+  
+  
 }
 
 getDogs();
